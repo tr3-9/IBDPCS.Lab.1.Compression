cc20e434ebcd28d70c3eb9acd79be5852931c0f4
Adam <adam.ewing.nz@gmail.com>
2025-10-06 06:35:47 +0800

    Lab uploaded.

--- src/main/java/org/example/RLE.java
+++ src/main/java/org/example/RLE.java
@@ -1,4 +1,37 @@
 package org.example;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class RLE {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        String decompressedString = textToString("src/main/resources/COVID-19");
+        String compressedString = compress(decompressedString);
+        System.out.println(compressedString);
+    }
+
+    /** This method converts the information stored in a text file into a String. */
+    public static String textToString(String filename) throws FileNotFoundException {
+        Scanner sc = new Scanner(new File(filename));
+        StringBuilder sb = new StringBuilder();
+        while (sc.hasNext()) {
+            String subSeq = sc.next();
+            for (int i = 0; i < subSeq.length(); i++) {
+                sb.append(subSeq.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+
+    /** TODO 1: Given a String (a genome sequence of COVID-19) implement the RLE algorithm that will use RLE to compress a String. Returns the compressed String. */
+    public static String compress(String uncompressed) { return null; }
+
+    /** TODO 2: Given a String (a genome sequence of COVID-19) implement the RLE algorithm that will use RLE to decompress a String. Returns the uncompressed String. */
+    public static String decompress(String compressed) {
+        return null;
+    }
+
+
 }
